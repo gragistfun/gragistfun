6f46c0e62ecb0582358e87dd80b2780b953a80a4
Karl Thomas <kt.utils@protonmail.com>
2021-01-11 21:00:41 +0100

    [dotfiles] Add color to man pages
    Either add escape characters to less pager, or set most as the MANPAGER
    and configer most to have less keybindings.

---
+++ dotfiles/zsh/.config/zsh/plugins/man_color.sh
@@ -0,0 +1,13 @@
+if command -v most > /dev/null; then
+	if [ -f /etc/redhat-release ]; then
+		export MANPAGER="most"
+	fi
+else
+	export LESS_TERMCAP_mb=$'\e[1;32m'
+	export LESS_TERMCAP_md=$'\e[1;32m'
+	export LESS_TERMCAP_me=$'\e[0m'
+	export LESS_TERMCAP_se=$'\e[0m'
+	export LESS_TERMCAP_so=$'\e[01;33m'
+	export LESS_TERMCAP_ue=$'\e[0m'
+	export LESS_TERMCAP_us=$'\e[1;4;31m'
+fi
