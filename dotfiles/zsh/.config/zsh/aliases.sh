8b36bf55c358ecaaa25a7b340d220f97b7d7dc08
Karl Thomas <kt.utils@protonmail.com>
2021-01-11 21:40:31 +0100

    [dotfiles] zsh - Add some alias
    Includes:
    	- Aliases for rm, cp, mv for safety
    	- Global aliases for convinence
    	- dotfiles alias for opening the setup directory in code

--- dotfiles/zsh/.config/zsh/aliases.sh
+++ dotfiles/zsh/.config/zsh/aliases.sh
@@ -1,3 +1,13 @@
 alias la='ls -lah'
 
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias -g G='| grep'
+alias -g H='| head'
+alias -g T='| tail'
+alias -g P='| less'
+
 alias plan="code -n $HOME/Cloud/plan/plan.md"
+alias dotfiles="code -n $HOME/.setup"
