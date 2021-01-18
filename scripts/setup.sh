e5b825e4f51a91c3176ba100fc0b1c9d3b6ddcc3
Karl Thomas <kt.utils@protonmail.com>
2021-01-18 18:45:05 +0100

    [scripts] setup - Make prerequisite installation automatic

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,11 @@
 echo "PREREQUISITES:"
 if [ -f /etc/fedora-release ]; then
     echo "Running prerequisites for FEDORA"
-    sudo dnf install ansible
+    sudo dnf -y install ansible
 else
     echo "ERROR: Script does not support OS or distribution!"
 fi
 
 echo
-echo "Staring configuration"
+echo "Starting configuration"
 ansible-pull -U https://github.com/ktutils/kt -d $HOME/.setup -K ansible/local.yml
