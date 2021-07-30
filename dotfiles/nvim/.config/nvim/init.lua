51c6a1ef6c20959efcb657eb6ac0e7c6163b3352
Karl Thomas <kt.utils@protonmail.com>
2021-07-30 19:53:40 +0200

    [dotfiles] neovim - Change init.vim into init.lua

---
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.undodir = '/tmp'
+vim.opt.undofile = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
+
+vim.opt.autoindent = true
+vim.opt.linebreak = false
