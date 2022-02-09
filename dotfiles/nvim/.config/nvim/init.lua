cb15595533c6fa040223d3ce93ed25aefad902a5
Karl Thomas <kt.utils@protonmail.com>
2022-02-09 15:01:31 +0100

    [dotfiles] nvim - Move options into seperate file

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -1,24 +1,5 @@
--- Options
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.undodir = '/tmp'
-vim.opt.undofile = true
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 5
-
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.ignorecase = true
-
-vim.opt.hidden = true
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
+require('user.options')
 
 -- Plugings
 require('plugins')
+
---
+++ dotfiles/nvim/.config/nvim/lua/user/options.lua
@@ -0,0 +1,23 @@
+local o = vim.opt
+
+-- Global options
+o.hidden = true
+
+o.backup = false
+o.swapfile = false
+o.undodir = '/tmp'
+o.undofile = true
+
+o.number = true
+o.relativenumber = true
+o.scrolloff = 5
+
+o.autoindent = true
+o.smartindent = true
+o.wrap = false
+o.ignorecase = true
+
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
+o.expandtab = true
