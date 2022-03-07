d503a40a1fa9bddc7b0d32cb75f56d8a4993ac33
gragistfun <gragistfun@protonmail.com>
2022-03-07 12:37:37 +0100

    [dotfiles] fish - Add git function
    Show status with only 'git' command.
    
    Navigate to root directory of git repo with 'git root'.

--- dotfiles/fish/.config/fish/conf.d/functions.fish
+++ dotfiles/fish/.config/fish/conf.d/functions.fish
@@ -2,6 +2,19 @@ function ccd
     mkdir $argv[1] && cd $argv[1]
 end
 
+function git
+    switch $argv[1]
+    case ""
+        command git status -sb --show-stash
+        return
+    case "root"
+        cd (git rev-parse --show-toplevel)
+        return
+    end
+
+    command git $argv
+end
+
 function scd 
     if test (count $argv) -eq 1
         cd (fd -t d -H -E .git --search-path $argv[1] | sk)
