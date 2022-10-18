c4fdaaf88c9dd8ea8dbb28e2da10ff7fc0c8008b
gragistfun <gragistfun@posteo.de>
2022-10-18 18:03:17 +0200

    refactor(nvim): switch nvim-tree repo
    Remove optional icon package

--- dotfiles/nvim/.config/nvim/lua/user/plugins.lua
+++ dotfiles/nvim/.config/nvim/lua/user/plugins.lua
@@ -51,13 +51,7 @@ return require('packer').startup(function(use)
   }
 
   -- File Explorer
-  use {	  
-    'kyazdani42/nvim-tree.lua',
-    requires = {{'kyazdani42/nvim-web-devicons'}},
-    config = function()
-        require('nvim-tree').setup {}
-    end
-  }
+  use 'nvim-tree/nvim-tree.lua'
 
   -- Git
   use 'tpope/vim-fugitive'
