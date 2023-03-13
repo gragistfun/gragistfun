efb373b593ecf6b9c1f8a8fdd07a6a2e6d9e32c0
gragistfun <gragistfun@posteo.de>
2023-03-13 17:27:41 +0100

    feat(nvim): add 'whick-key' package

--- dotfiles/nvim/.config/nvim/lua/user/plugins.lua
+++ dotfiles/nvim/.config/nvim/lua/user/plugins.lua
@@ -23,6 +23,16 @@ return require('packer').startup(function(use)
     end
   })
 
+  -- Which-key
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {}
+    end
+  }
+
   -- LSP
   use {
     'neovim/nvim-lspconfig',
